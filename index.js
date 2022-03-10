f28a17a03b80a9e04621e927c9d7ebdf710783a5
Pollyanna Ribeiro <23667108+PollyannaRibeiro@users.noreply.github.com>
2022-03-10 12:03:28 +0000

    adding bottom padding space between matches and bold in the winners

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 $.getJSON("wimbledon-men.json", function( data ) {
     let item = "";
+    let counter = 0;
     data["match"].forEach(match => {
 
         
         match["player"].forEach( player => {
             item += "<tr>"
             item += "<th>"+ match["round"] +"</th>"
-            item += "<td>"+ player["name"] + "</td>";
             
-            // console.log(player["outcome"])
+            if (player["outcome"] == "won"){
+                item += "<td class='winner'>"+ player["name"] + "</td>";
+            } else{
+                item += "<td>"+ player["name"] + "</td>";
+            }
+            counter +=1;
             // console.log(player["sets-won"])
             player["set"].forEach(set =>{
                 item += "<td>"+ set + "</td>";
             });
             item +="</tr>";
             $('tbody').append(item);
+            if (counter == 2){
+                $('tbody').append("<br/>");
+                counter = 0;
+            }
             item = "";
         });
     });
-    
-    
   });
 
 
