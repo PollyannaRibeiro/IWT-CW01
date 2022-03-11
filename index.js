36ec85f598c85cb0d9f2dfc1f122786c9c0d1e4c
Pollyanna Ribeiro <23667108+PollyannaRibeiro@users.noreply.github.com>
2022-03-11 23:42:40 +0000

    added playerChecked and implemented player name search tool

--- index.js
+++ index.js
@@ -1,59 +1,52 @@
 $( document ).ready(function() {
-  
     // form colecting info
-
     $( "form" ).submit(function( event ) {
         let newArray = $( this ).serializeArray()
         newArray.reverse();
-        console.log(  newArray);
         filteringData(newArray)
         event.preventDefault();
     });
 });
 
 function filteringData(array){
-    // console.log(array[0])
-    // console.log(array[0].name)
-    // console.log(array[0].value)
 
     let jsonLinks = ["wimbledon-men.json", "wimbledon-women.json"]
 
     // variable and conditions
-    let round = array[2].value;
-    console.log(round)
+    let roundRequested = array[2].value;
     let conditionRound =  $('#sel-round').val();
-    let player = array[1].value;
+    let playerRequested = array[1].value;
     let conditionPlayer =  $('#sel-player').val();
     let setRequested = array[0].value;
     let conditionSet =  $('#sel-set').val();
     let tournamentLink = [];
 
-    console.log("len" + array.length)
     // when needs to search in both files
     if (array.length === 3 || array.length == 5){
         tournamentLink = jsonLinks;
     }
     // when needs to search in only one file 
     if (array.length === 4){
-        let tournament = array[3].value;
-        
-        if(tournament == "men"){
+        if(array[3].value == "men"){
             tournamentLink = [jsonLinks[0]];
-
         } else{
             tournamentLink = [jsonLinks[1]];
         }  
     }
+
     tournamentLink.forEach(link => {
         $.getJSON(link, function( data ) {
             data["match"].forEach(match => {
                 let item = "";
-                let counter = 0;
                 let setChecked = false;
                 let playerChecked = false;
                 // if round respect the rules:
-                if(searchingRoundOrSet(round, conditionRound, match["round"])){
+                if(searchingRoundOrSet(roundRequested, conditionRound, match["round"])){
                     match["player"].forEach( player => {
+                        if(searchingName(playerRequested, conditionPlayer, player["name"])){
+                            playerChecked = true;
+                        }
+                        
                         item += "<tr>"
                         item += "<th>"+ match["round"] +"</th>"
                         
@@ -62,28 +55,29 @@ function filteringData(array){
                         } else{
                             item += "<td>"+ player["name"] + "</td>";
                         }
-                        counter +=1;
                         // console.log(player["sets-won"])             
                         player["set"].forEach(set =>{
                             if(searchingRoundOrSet(setRequested, conditionSet, set)){
                                 setChecked = true; 
                             }
-                            console.log("setChecked: " + setChecked);
                             item += "<td>"+ set + "</td>";
                         });
                         item +="</tr>";                      
                     });
-                    if(setChecked && counter == 2){
+                    if(setChecked  && playerChecked){
                         if(link == jsonLinks[0]){
                             $('#men-table tbody').append(item);
                             $('#men-table tbody').append("<br/>");
+                            $("#men-table").css("display", "contents");
+                            $("#men-title").css("display", "contents");
                         }
                         else {
                             $('#women-table tbody').append(item);
                             $('#women-table tbody').append("<br/>");
+                            $("#women-table").css("display", "contents");
+                            $("#women-title").css("display", "contents");
                         }
                     } 
-                    counter = 0;
                     item = "";
                 } 
             });
@@ -91,11 +85,11 @@ function filteringData(array){
     });
 }
     
-function searchinName(requested, condition, value){
-    if(requested == null){
+function searchingName(requested, condition, value){
+    if(requested == null || requested === ""){
         return true;
         // It's not searching this information, so it will pass all its content
-    }else{
+    } else {
         if(condition == "contains"){
             if(value.indexOf(requested) >= 0){
                 return true;
@@ -116,7 +110,7 @@ function searchinName(requested, condition, value){
 }
 
 function searchingRoundOrSet(requested, condition, value){
-    if(requested === null || requested===""){
+    if(requested === null || requested === ""){
         return true;
         // It's not searching this information, so it will pass all its content
     }else{
@@ -138,4 +132,3 @@ function searchingRoundOrSet(requested, condition, value){
         return false;
     }
 }
-
