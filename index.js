5a52504f0129cfb3dae1806d1a61fe556317b7b7
Pollyanna Ribeiro <23667108+PollyannaRibeiro@users.noreply.github.com>
2022-03-11 21:01:36 +0000

    added form submit and filteringData function (searching for the round is already working)

--- index.js
+++ index.js
@@ -1,33 +1,93 @@
-$.getJSON("wimbledon-men.json", function( data ) {
-    let item = "";
-    let counter = 0;
-    data["match"].forEach(match => {
+$( document ).ready(function() {
+  
+    // form colecting info
 
+    $( "form" ).submit(function( event ) {
+        let newArray = $( this ).serializeArray()
+        newArray.reverse();
+        console.log(  newArray);
+        separingData(newArray)
+        event.preventDefault();
+    });
+});
+
+function separingData(array){
+    // console.log(array[0])
+    // console.log(array[0].name)
+    // console.log(array[0].value)
+
+    let jsonLinks = ["wimbledon-men.json", "wimbledon-women.json"]
+
+    // variable and conditions
+    let round = array[2].value;
+    console.log(round)
+    let conditionRound =  $('#sel-round').val();
+    let player = array[1].value;
+    let conditionPlayer =  $('#sel-player').val();
+    let set = array[0].value;
+    let conditionSet =  $('#sel-set').val();
+    let tournamentLink = [];
+
+    console.log("len" + array.length)
+    // when needs to search in both files
+    if (array.length === 3 || array.length == 5){
+        tournamentLink = jsonLinks;
+    }
+    // when needs to search in only one file 
+    if (array.length === 4){
+        let tournament = array[3].value;
         
-        match["player"].forEach( player => {
-            item += "<tr>"
-            item += "<th>"+ match["round"] +"</th>"
-            
-            if (player["outcome"] == "won"){
-                item += "<td class='winner'>"+ player["name"] + "</td>";
-            } else{
-                item += "<td>"+ player["name"] + "</td>";
-            }
-            counter +=1;
-            // console.log(player["sets-won"])
-            player["set"].forEach(set =>{
-                item += "<td>"+ set + "</td>";
+        if(tournament == "men"){
+            tournamentLink = [jsonLinks[0]];
+
+        } else{
+            tournamentLink = [jsonLinks[1]];
+        }  
+    }
+    tournamentLink.forEach(link => {
+        $.getJSON(link, function( data ) {
+            data["match"].forEach(match => {
+                let item = "";
+                let counter = 0;
+                // if round respect the rules:
+                if(searchingRoundOrSet(round, conditionRound, match["round"])){
+                    match["player"].forEach( player => {
+                        item += "<tr>"
+                        item += "<th>"+ match["round"] +"</th>"
+                        
+                        if (player["outcome"] == "won"){
+                            item += "<td class='winner'>"+ player["name"] + "</td>";
+                        } else{
+                            item += "<td>"+ player["name"] + "</td>";
+                        }
+                        counter +=1;
+                        // console.log(player["sets-won"])
+                        player["set"].forEach(set =>{
+                            item += "<td>"+ set + "</td>";
+                        });
+                        item +="</tr>";
+                        if(link == jsonLinks[0]){
+                            $('#men-table tbody').append(item);
+                            if (counter == 2){
+                                $('#men-table tbody').append("<br/>");
+                                counter = 0;
+                            }
+                        }
+                        else{
+                            $('#women-table tbody').append(item);
+                            if (counter == 2){
+                                $('#women-table tbody').append("<br/>");
+                                counter = 0;
+                            }
+
+                        }
+                        item = "";
+                    });
+
+                } 
             });
-            item +="</tr>";
-            $('tbody').append(item);
-            if (counter == 2){
-                $('tbody').append("<br/>");
-                counter = 0;
-            }
-            item = "";
         });
     });
-  });
 }
     
 function searchinName(requested, condition, value){
